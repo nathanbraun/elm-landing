ebbbcfaee555e71916b8e92582cd2d8d30b75c2f
Nathan Braun <nathanericbraun@gmail.com>
2021-04-15 15:19:50 -0500

    make more general purpose

--- Common.elm
+++ Common.elm
@@ -1,9 +1,6 @@
 module Common exposing
-    ( Logo
-    , gray
-    , noPadding
-    , noto
-    , source
+    ( Fonts
+    , Image
     , viewEmail
     , viewGrid
     , viewHeader
@@ -23,18 +20,26 @@ import Widget.Customize as Customize
 import Widget.Material as Material
 
 
-gray : Color
-gray =
-    Element.rgb255 149 165 166
+type alias Image =
+    { src : String
+    , desc : String
+    }
 
 
-noto : Element.Attribute msg
-noto =
-    Font.family [ Font.typeface "Noto Sans JP" ]
+type alias Fonts msg =
+    { main : Element.Attribute msg
+    , secondary : Element.Attribute msg
+    }
 
 
-viewHeader : Int -> Device -> List String -> Maybe (List String) -> Element msg
-viewHeader h device desktopText mobileText =
+viewHeader :
+    Int
+    -> List (Element.Attribute msg)
+    -> Device
+    -> List String
+    -> Maybe (List String)
+    -> Element msg
+viewHeader h style device desktopText mobileText =
     let
         textList =
             case ( device.class, device.orientation ) of
@@ -63,48 +68,45 @@ viewHeader h device desktopText mobileText =
                     20
     in
     column
-        [ Font.size fontSize
-        , Font.semiBold
-        , centerX
-        , Font.center
-        , spacing 10
-        , Font.family [ Font.typeface "Noto Sans JP" ]
-        ]
+        ([ Font.size fontSize
+         , centerX
+         , Font.center
+         , spacing 10
+         ]
+            ++ style
+        )
         (List.map (\x -> paragraph [] [ text x ]) textList)
 
 
-viewText : Device -> Int -> String -> Element msg
-viewText device max text_ =
+viewText : List (Element.Attribute msg) -> Device -> Int -> String -> Element msg
+viewText style device max text_ =
     column [ centerX, width (fill |> maximum max) ]
         [ paragraph
-            [ source
-            , Font.center
-            , Font.color (Element.rgb255 149 165 166)
-            ]
+            ([ Font.center ] ++ style)
             [ text text_ ]
         ]
 
 
-source : Element.Attribute msg
-source =
-    Font.family [ Font.typeface "Source Serif Pro" ]
-
-
-viewEmail : Device -> { model | email : String } -> (String -> msg) -> Element msg
-viewEmail device model msg =
+viewEmail :
+    Fonts msg
+    -> Device
+    -> { model | email : String }
+    -> (String -> msg)
+    -> Element msg
+viewEmail fonts device model msg =
     let
         desktopView =
             column [ width fill, paddingXY 0 50, spacing 10 ]
                 [ row [ centerX, spacing 20, width (fill |> maximum 450) ]
-                    [ el [ width fill ] (viewLoc model msg)
-                    , el [ centerX ] (viewButton "Try It!")
+                    [ el [ width fill ] (viewLoc fonts.secondary model msg)
+                    , el [ centerX ] (viewButton fonts.main "Try It!")
                     ]
                 ]
 
         mobileView =
             column [ centerX, spacing 10, width (fill |> maximum 500), paddingXY 0 20 ]
-                [ el [ width fill, paddingXY 10 0 ] (viewLoc model msg)
-                , el [ centerX, paddingXY 0 0 ] (viewButton "Try It!")
+                [ el [ width fill, paddingXY 10 0 ] (viewLoc fonts.secondary model msg)
+                , el [ centerX, paddingXY 0 0 ] (viewButton fonts.main "Try It!")
                 ]
     in
     case ( device.class, device.orientation ) of
@@ -118,22 +120,29 @@ viewEmail device model msg =
             mobileView
 
 
-viewLoc : { model | email : String } -> (String -> msg) -> Element msg
-viewLoc model msg =
+viewLoc :
+    Element.Attribute msg
+    -> { model | email : String }
+    ->
+        (String
+         -> msg
+        )
+    -> Element msg
+viewLoc font model msg =
     Widget.textInput (Material.textInput Material.defaultPalette |> Customize.elementRow [ width fill ])
         { chips = []
         , text = model.email
         , placeholder =
             Just <|
                 Input.placeholder [] <|
-                    el [ source ] (Element.text "Email")
+                    el [ font ] (Element.text "Email")
         , onChange = msg
         , label = "Location"
         }
 
 
-viewButton : String -> Element msg
-viewButton label =
+viewButton : Element.Attribute msg -> String -> Element msg
+viewButton font label =
     Input.button
         [ centerX ]
         { onPress = Nothing
@@ -144,28 +153,26 @@ viewButton label =
                 , paddingXY 30 15
                 , Background.color (rgb255 52 152 219)
                 , Font.color (rgb255 255 255 255)
-                , Font.family
-                    [ Font.typeface "Noto Sans JP"
-                    ]
+                , font
                 ]
                 (text label)
         }
 
 
-viewMaxImage : Int -> String -> String -> Element ms
-viewMaxImage max src desc =
+viewMaxImage : Int -> Image -> Element ms
+viewMaxImage max img =
     image
         [ centerX
         , Element.alignTop
         , width (fillPortion 1 |> maximum max)
         ]
-        { src = src
-        , description = desc
+        { src = img.src
+        , description = img.desc
         }
 
 
-viewImageText : String -> String -> String -> Element msg
-viewImageText caption src desc =
+viewImageText : String -> Image -> Element msg
+viewImageText caption img =
     Widget.itemList
         (Material.cardColumn Material.defaultPalette)
         [ Widget.asItem <|
@@ -184,40 +191,29 @@ viewImageText caption src desc =
                 )
         , Widget.divider (Material.middleDivider Material.defaultPalette)
         , Widget.asItem <|
-            viewMaxImage 250 src desc
+            viewMaxImage 250 (Image img.src img.desc)
         ]
 
 
-type alias Logo =
-    { src : String
-    , desc : String
-    }
-
-
-viewLogos : List Logo -> Element msg
-viewLogos logos =
+viewLogos : List (Element.Attribute msg) -> List Image -> Element msg
+viewLogos textStyle logos =
     column
         [ width fill
         , paddingXY 0 40
         , Background.color (Element.rgb255 236 240 241)
         , spacing 20
-        , Font.color gray
         ]
-        [ el [ centerX, noto ] (text "MADE WITH:")
+        [ el ([ centerX ] ++ textStyle) (text "MADE WITH:")
         , row
             [ spacing 20
             , centerX
             ]
-            (List.map (\{ src, desc } -> viewMaxImage 40 src desc) logos)
+            (List.map (\{ src, desc } -> viewMaxImage 40 (Image src desc)) logos)
         ]
 
 
-noPadding =
-    { top = 0, bottom = 0, right = 0, left = 0 }
-
-
-viewGrid : Device -> Element msg
-viewGrid device =
+viewGrid : Element.Attribute msg -> Device -> Element msg
+viewGrid font device =
     let
         style =
             case ( device.class, device.orientation ) of
@@ -230,13 +226,13 @@ viewGrid device =
                 _ ->
                     [ alignTop ]
     in
-    column [ centerX, source, paddingXY 5 0 ]
+    column [ centerX, font, paddingXY 5 0 ]
         [ wrappedRow []
-            [ el style (viewImageText "1. Read your book, either on your Kindle or in print." "/images/f3wt1_58.jpg" "1")
-            , el style (viewImageText "2. When you spot something interesting, type the page number and record a quick audio note." "/images/record_screenshot.png" "2")
+            [ el style (viewImageText "1. Read your book, either on your Kindle or in print." (Image "/images/f3wt1_58.jpg" "1"))
+            , el style (viewImageText "2. When you spot something interesting, type the page number and record a quick audio note." (Image "/images/record_screenshot.png" "2"))
             ]
         , wrappedRow []
-            [ el style (viewImageText "3. Need to follow up? Record as many subnotes as you want." "/images/sub_screenshot.png" "2")
-            , el style (viewImageText "4. Export your notes to plain text for editing when you're done." "/images/final_58.jpg" "2")
+            [ el style (viewImageText "3. Need to follow up? Record as many subnotes as you want." (Image "/images/sub_screenshot.png" "2"))
+            , el style (viewImageText "4. Export your notes to plain text for editing when you're done." (Image "/images/final_58.jpg" "2"))
             ]
         ]
